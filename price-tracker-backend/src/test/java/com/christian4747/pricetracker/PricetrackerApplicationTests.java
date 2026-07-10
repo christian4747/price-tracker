b69a8b24082b621105a18c10cb3fd256876f75de
christian4747 <christianan711+2@gmail.com>
2026-07-11 03:02:00 +0900

    Added starter backend files

---
+++ price-tracker-backend/src/test/java/com/christian4747/pricetracker/PricetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christian4747.pricetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
