26f3160cf40a6336080f79c452ffa969e7ffe97b
christian4747 <christianan711+2@gmail.com>
2026-07-15 04:41:55 +0900

    Added ProductService tests and improved PriceService tests

--- price-tracker-backend/src/test/java/com/christian4747/pricetracker/services/PriceServiceTest.java
+++ price-tracker-backend/src/test/java/com/christian4747/pricetracker/services/PriceServiceTest.java
@@ -5,6 +5,7 @@ import com.christian4747.pricetracker.daos.ProductDAO;
 import com.christian4747.pricetracker.models.Price;
 import com.christian4747.pricetracker.models.Product;
 import com.christian4747.pricetracker.models.dtos.IncomingPriceDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,14 +14,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,24 +38,39 @@ public class PriceServiceTest {
     @Mock
     private Page<Price> pricePage;
 
+    @Mock
+    private Pageable pageable;
+
+    private IncomingPriceDTO priceDTO;
+    private Price price;
+    private Product product;
+    private List<Price> priceList;
+
+    @BeforeEach
+    public void setUp() {
+        priceDTO = new IncomingPriceDTO(1, null, null, null, 1);
+
+        product = new Product();
+        product.setProductId(1);
+
+        price = new Price(1, 1, null, null, null, null, null, product);
+
+        Price price1 = new Price(1, 1, null, null, null, null, null, null);
+        Price price2 = new Price(2, 2, null, null, null, null, null, null);
+        Price price3 = new Price(3, 3, null, null, null, null, null, null);
+
+        priceList = List.of(price1, price2, price3);
+    }
+
     @Test
     public void addPrice_nonExistentProduct_throwException() {
-        IncomingPriceDTO price = new IncomingPriceDTO(1, null, null, null, 1);
-
         when(productDAO.findById(anyInt())).thenReturn(Optional.empty());
 
-        assertThrows(IllegalArgumentException.class, () -> priceService.addPrice(price));
+        assertThrows(IllegalArgumentException.class, () -> priceService.addPrice(priceDTO));
     }
 
     @Test
     public void addPrice_validPrice_returnPrice() {
-        IncomingPriceDTO priceDTO = new IncomingPriceDTO(1, null, null, null, 1);
-
-        Product product = new Product();
-        product.setProductId(1);
-
-        Price price = new Price(1, 1, null, null, null, null, null, product);
-
         when(productDAO.findById(anyInt())).thenReturn(Optional.of(product));
         when(priceDAO.save(any())).thenReturn(price);
 
@@ -64,8 +79,6 @@ public class PriceServiceTest {
 
     @Test
     public void deletePrice_existingPrice_returnPrice() {
-        Price price = new Price(1, 1, null, null, null, null, null, null);
-
         when(priceDAO.findById(any())).thenReturn(Optional.of(price));
 
         assertEquals(1, priceService.deletePrice(1).getPriceId());
@@ -80,27 +93,14 @@ public class PriceServiceTest {
 
     @Test
     public void getAllPrices_threePrices_returnThreePrices() {
-        Price price1 = new Price(1, 1, null, null, null, null, null, null);
-        Price price2 = new Price(2, 2, null, null, null, null, null, null);
-        Price price3 = new Price(3, 3, null, null, null, null, null, null);
-
-        ArrayList<Price> priceList = new ArrayList<>();
-        priceList.add(price1);
-        priceList.add(price2);
-        priceList.add(price3);
-
         when(pricePage.getContent()).thenReturn(priceList);
         when(priceDAO.findAll(any(Pageable.class))).thenReturn(pricePage);
-
-        Pageable pageable = mock(Pageable.class);
 
         assertEquals(3, priceService.getAllPrices(pageable).size());
     }
 
     @Test
     public void getPriceById_existingPrice_returnPrice() {
-        Price price = new Price(1, 1, null, null, null, null, null, null);
-
         when(priceDAO.findById(any())).thenReturn(Optional.of(price));
 
         assertEquals(1, priceService.getPriceById(1).getPriceId());
@@ -115,8 +115,6 @@ public class PriceServiceTest {
 
     @Test
     public void updatePrice_nonExistentPrice_throwException() {
-        IncomingPriceDTO priceDTO = new IncomingPriceDTO(1, null, null, null, 1);
-
         when(priceDAO.findById(anyInt())).thenReturn(Optional.empty());
 
         assertThrows(IllegalArgumentException.class, () -> priceService.updatePrice(1, priceDTO));
@@ -124,27 +122,16 @@ public class PriceServiceTest {
 
     @Test
     public void updatePrice_nonExistentProduct_throwException() {
-        IncomingPriceDTO priceDTO = new IncomingPriceDTO(1, null, null, null, 2);
-
-        Product product = new Product();
-        product.setProductId(1);
-
-        Price price = new Price(1, 1, null, null, null, null, null, product);
+        priceDTO = new IncomingPriceDTO(1, null, null, null, 2);
 
         when(priceDAO.findById(anyInt())).thenReturn(Optional.of(price));
         when(productDAO.findById(anyInt())).thenReturn(Optional.empty());
+
         assertThrows(IllegalArgumentException.class, () -> priceService.updatePrice(1, priceDTO));
     }
 
     @Test
     public void updatePrice_validPrice_returnNewPrice() {
-        IncomingPriceDTO priceDTO = new IncomingPriceDTO(1, null, null, null, 1);
-
-        Product product = new Product();
-        product.setProductId(1);
-
-        Price price = new Price(1, 1, null, null, null, null, null, product);
-
         when(priceDAO.findById(anyInt())).thenReturn(Optional.of(price));
         when(priceDAO.save(any())).thenReturn(price);
 
@@ -154,11 +141,6 @@ public class PriceServiceTest {
     @Test
     public void updatePrice_validPriceDiffProduct_returnNewPrice() {
         IncomingPriceDTO priceDTO = new IncomingPriceDTO(1, null, null, null, 2);
-
-        Product product = new Product();
-        product.setProductId(1);
-
-        Price price = new Price(1, 1, null, null, null, null, null, product);
 
         Product product2 = new Product();
         product2.setProductId(2);
