be5b5aab6458206f7c75d3e6877c5db185692fd6
christian4747 <christian.andersen4747@gmail.com>
2026-07-22 11:34:48 +0900

    Added custom tooltip to PriceHistoryChart

--- price-tracker-frontend/src/components/home/price/PriceHistoryChart.tsx
+++ price-tracker-frontend/src/components/home/price/PriceHistoryChart.tsx
@@ -1,7 +1,27 @@
-import { Line, LineChart, Tooltip, XAxis } from 'recharts'
+import { Line, LineChart, Tooltip, XAxis, type TooltipContentProps } from 'recharts'
 
 type Props = {
     priceData: any[]
+}
+
+const PriceHistoryTooltip = ({active, payload, label}: TooltipContentProps) => {
+    const firstPayload = payload?.[0];
+    const isVisible = active && firstPayload != null;
+    return (
+        <div
+            className="custom-tooltip"
+            style={{
+                visibility: isVisible ? 'visible' : 'hidden'
+            }}
+        >
+        {isVisible && (
+            <div className='m-0 p-3 bg-[#F4F4F4] border-1 border-[#BCBBBD] rounded-sm whitespace-nowrap flex flex-col'>
+                <p className="label">{`${label}`}</p>
+                <p>{`Price: $${firstPayload.value}`}</p>
+            </div>
+        )}
+        </div>
+    );
 }
 
 const PriceHistoryChart = ({priceData}: Props) => {
@@ -9,8 +29,8 @@ const PriceHistoryChart = ({priceData}: Props) => {
         <div className='w-7/10 border-1 border-[#BCBBBD] rounded-sm p-1'>
             <LineChart style={{ width: '100%', aspectRatio: 3}} responsive data={priceData}>
                 <XAxis dataKey="priceStarted" />
-                <Line type="stepAfter" dataKey="price" />
-                <Tooltip />
+                <Line  dataKey="price" />
+                <Tooltip content={PriceHistoryTooltip} />
             </LineChart>
         </div>
     )
