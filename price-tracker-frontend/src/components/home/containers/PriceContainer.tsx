cc586b3191f4f885737f6d4a20715745dfa703a1
christian4747 <christian.andersen4747@gmail.com>
2026-07-27 03:59:21 +0900

    Move edit price logic into custom hook

--- price-tracker-frontend/src/components/home/containers/PriceContainer.tsx
+++ price-tracker-frontend/src/components/home/containers/PriceContainer.tsx
@@ -3,68 +3,26 @@ import DeletePriceModal from '../modals/DeletePriceModal'
 import type { PriceType, ProductType } from "../../../utils/Types"
 import Price from '../price/Price'
 import api from '../../../services/api'
-import { getUSDateStringFromTimestamp, javaTimestampToJS } from '../../../utils/DateUtilities'
+import { getUSDateStringFromTimestamp } from '../../../utils/DateUtilities'
 import { useToggleVisibility } from '../../../hooks/useToggleVisibility'
-import { usePriceDTO } from '../../../hooks/usePriceDTO'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useEditPrice } from '../../../hooks/useEditPrice'
 
 type PriceProps = {
     price: PriceType
 }
 
 const PriceContainer = ({price}: PriceProps) => {
-    // State for EditPriceModal visibility
-    const showEditPrice = useToggleVisibility(false)
     // State for DeletePriceModal visibility
     const showDeletePrice = useToggleVisibility(false)
 
-    // State for PriceDTO for editing Prices
-    const priceDTO = usePriceDTO(
-        {
-            amount: parseFloat(price.amount).toFixed(2),
-            currency: price.currency || '',
-            priceStarted: price.priceStarted ? javaTimestampToJS(price.priceStarted) : '',
-            priceEnded: price.priceEnded? javaTimestampToJS(price.priceEnded) : '',
-            productId: price.productId
-        }
-    )
+    const editPrice = useEditPrice(price)
+ 
     // Constructing the string to show the date on the PriceList
     const priceStartedDateString = getUSDateStringFromTimestamp(price.priceStarted)
 
     // Get the query client
     const queryClient = useQueryClient()
-
-    // Mutation for editing prices
-    const editPriceMutation = useMutation({
-        mutationFn: () => {
-            showEditPrice.toggle()
-            return api.editPrice(price.priceId, priceDTO.value)
-        },
-        onSuccess: (newData) => {
-            queryClient.setQueryData(['products'], (old: any) => {
-                return old.map((p: ProductType) => {
-                    if (p.productId === price.productId) {
-                        const priceIdx = p.prices.indexOf(price)
-
-                        const updatedPrices = p.prices.map((price, i) => {
-                            if (i === priceIdx) {
-                                return newData
-                            }
-                            return price
-                        })
-
-                        let productCopy = {...p}
-                        productCopy = {...productCopy, prices: updatedPrices}
-                        return productCopy
-                    }
-                    return p
-                })
-            })
-        },
-        onError: (error) => {
-            console.log(`Error occurred while updating ${price.productId}: ${price.amount} (${error.message})`)
-        }
-    })
 
     // Mutation for deleting prices
     const deletePriceMutation = useMutation({
@@ -103,16 +61,16 @@ const PriceContainer = ({price}: PriceProps) => {
             <Price
                 price={price}
                 priceStartedDateString={priceStartedDateString}
-                toggleShowEditPrice={showEditPrice.toggle}
+                toggleShowEditPrice={editPrice.visibility.toggle}
                 toggleShowDeletePrice={showDeletePrice.toggle}
             />
 
             <EditPriceModal
-                hidden={showEditPrice.value}
-                toggleHidden={showEditPrice.toggle}
-                editPrice={editPriceMutation.mutate}
-                priceDTO={priceDTO.value}
-                setPriceDTO={priceDTO.setPriceDTO}
+                hidden={editPrice.visibility.value}
+                toggleHidden={editPrice.visibility.toggle}
+                editPrice={editPrice.mutation.mutate}
+                priceDTO={editPrice.priceDTO.value}
+                setPriceDTO={editPrice.priceDTO.setPriceDTO}
             />
 
             <DeletePriceModal
---
+++ price-tracker-frontend/src/hooks/useEditPrice.ts
@@ -0,0 +1,66 @@
+import type { PriceType, ProductType } from '../utils/Types'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import api from '../services/api'
+import { useToggleVisibility } from './useToggleVisibility'
+import { usePriceDTO } from './usePriceDTO'
+import { javaTimestampToJS } from '../utils/DateUtilities'
+
+
+export function useEditPrice(price: PriceType) {
+    // EditPriceModal visibility
+    const showEditPrice = useToggleVisibility(false)
+
+    // PriceDTO for editing
+    const priceDTO = usePriceDTO(
+        {
+            amount: parseFloat(price.amount).toFixed(2),
+            currency: price.currency || '',
+            priceStarted: price.priceStarted ? javaTimestampToJS(price.priceStarted) : '',
+            priceEnded: price.priceEnded? javaTimestampToJS(price.priceEnded) : '',
+            productId: price.productId
+        }
+    )
+
+    // Get the query client
+    const queryClient = useQueryClient()
+
+    // Mutation for editing prices
+    const editPriceMutation = useMutation({
+        mutationFn: () => {
+            showEditPrice.toggle()
+            return api.editPrice(price.priceId, priceDTO.value)
+        },
+        onSuccess: (newData) => {
+            queryClient.setQueryData(['products'], (old: any) => {
+                return old.map((p: ProductType) => {
+                    if (p.productId === price.productId) {
+                        const priceIdx = p.prices.indexOf(price)
+
+                        const updatedPrices = p.prices.map((price, i) => {
+                            if (i === priceIdx) {
+                                return newData
+                            }
+                            return price
+                        })
+
+                        let productCopy = {...p}
+                        productCopy = {...productCopy, prices: updatedPrices}
+                        return productCopy
+                    }
+                    return p
+                })
+            })
+        },
+        onError: (error) => {
+            console.log(`Error occurred while updating ${price.productId}: ${price.amount} (${error.message})`)
+        }
+    })
+
+    const useEditPriceProps = {
+        visibility: showEditPrice,
+        mutation: editPriceMutation,
+        priceDTO: priceDTO
+    }
+
+    return useEditPriceProps
+}
