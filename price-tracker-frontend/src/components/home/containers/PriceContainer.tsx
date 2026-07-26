8639c94dcd92f6e95d7b6b6917069c53acfa7461
christian4747 <christian.andersen4747@gmail.com>
2026-07-27 04:03:35 +0900

    Move delete price logic into custom hook

--- price-tracker-frontend/src/components/home/containers/PriceContainer.tsx
+++ price-tracker-frontend/src/components/home/containers/PriceContainer.tsx
@@ -1,60 +1,24 @@
 import EditPriceModal from '../modals/EditPriceModal'
 import DeletePriceModal from '../modals/DeletePriceModal'
-import type { PriceType, ProductType } from "../../../utils/Types"
+import type { PriceType } from "../../../utils/Types"
 import Price from '../price/Price'
-import api from '../../../services/api'
 import { getUSDateStringFromTimestamp } from '../../../utils/DateUtilities'
-import { useToggleVisibility } from '../../../hooks/useToggleVisibility'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useEditPrice } from '../../../hooks/useEditPrice'
+import { useDeletePrice } from '../../../hooks/useDeletePrice'
 
 type PriceProps = {
     price: PriceType
 }
 
 const PriceContainer = ({price}: PriceProps) => {
-    // State for DeletePriceModal visibility
-    const showDeletePrice = useToggleVisibility(false)
 
+    // Hook for editing prices
     const editPrice = useEditPrice(price)
- 
+    // Hook for deleting prices
+    const deletePrice = useDeletePrice(price)
+
     // Constructing the string to show the date on the PriceList
     const priceStartedDateString = getUSDateStringFromTimestamp(price.priceStarted)
-
-    // Get the query client
-    const queryClient = useQueryClient()
-
-    // Mutation for deleting prices
-    const deletePriceMutation = useMutation({
-        mutationFn: () => {
-            showDeletePrice.toggle()
-            return api.deletePrice(price.priceId)
-        },
-        onSuccess: () => {
-            queryClient.setQueryData(['products'], (old: any) => {
-                return old.map((p: ProductType) => {
-                    if (p.productId === price.productId) {
-                        const priceIdx = p.prices.indexOf(price)
-
-                        const updatedPrices = p.prices.filter((price, i) => {
-                            if (i === priceIdx) {
-                                return
-                            }
-                            return price
-                        })
-
-                        let productCopy = {...p}
-                        productCopy = {...productCopy, prices: updatedPrices}
-                        return productCopy
-                    }
-                    return p
-                })
-            })
-        },
-        onError: (error) => {
-            console.log(`Error occurred while deleting ${price.priceId}: ${price.amount} ${price.priceStarted} (${error.message})`)
-        }
-    })
 
     return (
         <>
@@ -62,7 +26,7 @@ const PriceContainer = ({price}: PriceProps) => {
                 price={price}
                 priceStartedDateString={priceStartedDateString}
                 toggleShowEditPrice={editPrice.visibility.toggle}
-                toggleShowDeletePrice={showDeletePrice.toggle}
+                toggleShowDeletePrice={deletePrice.visibility.toggle}
             />
 
             <EditPriceModal
@@ -74,10 +38,10 @@ const PriceContainer = ({price}: PriceProps) => {
             />
 
             <DeletePriceModal
-                hidden={showDeletePrice.value}
-                toggleHidden={showDeletePrice.toggle}
+                hidden={deletePrice.visibility.value}
+                toggleHidden={deletePrice.visibility.toggle}
                 price={price}
-                deletePrice={deletePriceMutation.mutate}
+                deletePrice={deletePrice.mutation.mutate}
             />
         </>
     )
---
+++ price-tracker-frontend/src/hooks/useDeletePrice.ts
@@ -0,0 +1,51 @@
+import type { PriceType, ProductType } from '../utils/Types'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import api from '../services/api'
+import { useToggleVisibility } from './useToggleVisibility'
+
+export function useDeletePrice(price: PriceType) {
+    // DeletePriceModal visibility
+    const showDeletePrice = useToggleVisibility(false)
+
+    // Get the query client
+    const queryClient = useQueryClient()
+
+    // Mutation for deleting prices
+    const deletePriceMutation = useMutation({
+        mutationFn: () => {
+            showDeletePrice.toggle()
+            return api.deletePrice(price.priceId)
+        },
+        onSuccess: () => {
+            queryClient.setQueryData(['products'], (old: any) => {
+                return old.map((p: ProductType) => {
+                    if (p.productId === price.productId) {
+                        const priceIdx = p.prices.indexOf(price)
+
+                        const updatedPrices = p.prices.filter((price, i) => {
+                            if (i === priceIdx) {
+                                return
+                            }
+                            return price
+                        })
+
+                        let productCopy = {...p}
+                        productCopy = {...productCopy, prices: updatedPrices}
+                        return productCopy
+                    }
+                    return p
+                })
+            })
+        },
+        onError: (error) => {
+            console.log(`Error occurred while deleting ${price.priceId}: ${price.amount} ${price.priceStarted} (${error.message})`)
+        }
+    })
+
+    const useEditPriceProps = {
+        visibility: showDeletePrice,
+        mutation: deletePriceMutation
+    }
+
+    return useEditPriceProps
+}
