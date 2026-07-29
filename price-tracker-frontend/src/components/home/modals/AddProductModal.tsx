12d6c8f22eca1f196861f7436d121e777b358d77
christian4747 <christian.andersen4747@gmail.com>
2026-07-30 07:52:28 +0900

    Improve add product modal design

--- price-tracker-frontend/src/components/home/modals/AddProductModal.tsx
+++ price-tracker-frontend/src/components/home/modals/AddProductModal.tsx
@@ -2,6 +2,8 @@ import Button from "../../common/Button"
 import Input from "../../common/Input"
 import Modal from "../../common/Modal"
 import type { ModalProps, ProductDTO } from "../../../utils/Types";
+import ModalHeader from "../../common/ModalHeader";
+import LabeledInput from "../../common/LabeledInput";
 
 type AddProductModalProps = ModalProps & {
     product: ProductDTO,
@@ -14,29 +16,32 @@ const AddProductModal = ({hidden, toggleHidden, product, addProduct, setProductD
         <Modal
             hidden={hidden}
         >
-            <div className="text-4xl font-mono font-bold flex justify-center">Add Product</div>
-            <Input
+            <ModalHeader toggleHidden={toggleHidden}>Add Product</ModalHeader>
+            <LabeledInput
+                label="Name"
                 placeholder="Name"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setProductDTO(prev => ({...prev, name: e.target.value}))}}
                 value={product.name}
+                required
             />
-            <Input 
-                placeholder="Link"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setProductDTO(prev => ({...prev, link: e.target.value}))}}
-                value={product.link}
-            />
-            <Input
+            <LabeledInput
+                label="Store"
                 placeholder="Store"
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setProductDTO(prev => ({...prev, store: e.target.value}))}}
                 value={product.store}
+            />
+            <LabeledInput
+                label="Link"
+                placeholder="Link"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setProductDTO(prev => ({...prev, link: e.target.value}))}}
+                value={product.link}
             />
             <Input
                 className="hidden"
                 placeholder="Initial Price"
             />
             <div className="flex gap-2 justify-center">
-                <Button onClick={addProduct}>Save</Button>
-                <Button onClick={toggleHidden}>Cancel</Button>
+                <Button className="w-full" onClick={addProduct}>Add Product</Button>
             </div>
         </Modal>
     )
