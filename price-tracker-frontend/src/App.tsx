f4f23a1cf7619d199e525276f178cd48fe8e3f47
christian4747 <christian.andersen4747@gmail.com>
2026-08-07 05:41:13 +0900

    Add Inter font

--- price-tracker-frontend/src/App.tsx
+++ price-tracker-frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import { MantineProvider } from '@mantine/core'
+import { createTheme, MantineProvider } from '@mantine/core'
 import './App.css'
 import Home from './pages/HomePage'
 import '@mantine/core/styles.css'
 
+const theme = createTheme({
+    fontFamily: 'Inter, sans-serif'
+});
+
 function App() {
     return (
-        <MantineProvider>
+        <MantineProvider theme={theme}>
             <Home/>
         </MantineProvider>
     )
