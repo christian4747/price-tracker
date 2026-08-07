c78b0cf3dd51bde5fbdd6b613702bcebb2325df6
christian4747 <christian.andersen4747@gmail.com>
2026-08-07 15:21:33 +0900

    Add default button styling

--- price-tracker-frontend/src/App.tsx
+++ price-tracker-frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import { createTheme, MantineProvider } from '@mantine/core'
+import { Button, createTheme, MantineProvider } from '@mantine/core'
 import './App.css'
 import Home from './pages/HomePage'
 import '@mantine/core/styles.css'
 
 const theme = createTheme({
-    fontFamily: 'Inter, sans-serif'
+    fontFamily: 'Inter, sans-serif',
+    components: {
+        Button: Button.extend({
+            defaultProps: {
+                radius: 'xl',
+                color: 'var(--color-raisin)'
+            },
+        }),
+    },
 });
 
 function App() {
