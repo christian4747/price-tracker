a8173e46bb99a85d5a35535a9f96fe3b64b0e89c
christian4747 <christian.andersen4747@gmail.com>
2026-08-07 15:28:13 +0900

    Add price utils file

---
+++ price-tracker-frontend/src/utils/PriceUtilities.ts
@@ -0,0 +1,17 @@
+import type { PriceType } from "./Types"
+
+// Get the highest price from the price list
+const getHighestPrice = (prices: PriceType[]) => {
+    if (prices.length <= 0) return 0
+    let highest = parseFloat(prices[0].amount)
+
+    for (const price of prices) {
+        const priceVal = parseFloat(price.amount)
+        if (priceVal > highest) {
+            highest = priceVal
+        }
+    }
+    return highest
+}
+
+export { getHighestPrice }
