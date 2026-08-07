e51b14d02b6a08a5cbbba942bed586728054d8c1
christian4747 <christian.andersen4747@gmail.com>
2026-08-07 15:28:03 +0900

    Modify default input style

--- price-tracker-frontend/src/components/common/Input.tsx
+++ price-tracker-frontend/src/components/common/Input.tsx
@@ -5,8 +5,7 @@ type Props = React.ComponentPropsWithoutRef<"input"> & InputProps
 const InputWrapper = ({className, ...props}: Props) => {
     return (
         <Input
-            radius='xl'
-            className={"border border-smoke rounded-sm p-1 w-full " + className}
+            className={"w-full " + className}
             {...props}
         />
     )
