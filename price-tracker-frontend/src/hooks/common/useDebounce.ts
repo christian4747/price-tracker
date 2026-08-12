88fdc40ee11acc1b274be1a7559f31f3fbafe6a0
christian4747 <christian.andersen4747@gmail.com>
2026-08-13 07:25:37 +0900

    Add comment and tweak formatting

--- price-tracker-frontend/src/hooks/common/useDebounce.ts
+++ price-tracker-frontend/src/hooks/common/useDebounce.ts
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react'
 
+// Uses debounce to set a value and ignore inputs for the delay time
 export function useDebounce<T>(initialValue: T, delay: number = 5000) {
+
     const [value, setValue] = useState(initialValue)
     const debounce = useRef(false)
 
