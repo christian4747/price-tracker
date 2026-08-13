ac627a7a1ecb7e15434ab1016cec08681ef271ec
christian4747 <christian.andersen4747@gmail.com>
2026-08-13 14:25:16 +0900

    Use shorter date strings for graph

--- price-tracker-frontend/src/hooks/price/usePriceData.ts
+++ price-tracker-frontend/src/hooks/price/usePriceData.ts
@@ -22,7 +22,7 @@ export function usePriceData(dateToday: Date) {
 
                 return {
                     priceId: price.priceId,
-                    priceStarted: localizeFormatDayjs(getLocalDateFromUTC(new Date(price.priceStarted)), 'llll'),
+                    priceStarted: localizeFormatDayjs(getLocalDateFromUTC(new Date(price.priceStarted)), 'll'),
                     price: price.amount,
                     description: price.description
                 }
