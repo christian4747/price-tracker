69028531884d00043bda730c71630543631c31d2
christian4747 <christian.andersen4747@gmail.com>
2026-08-13 14:01:19 +0900

    Add util func to get local dates from UTC

--- price-tracker-frontend/src/utils/DateUtilities.ts
+++ price-tracker-frontend/src/utils/DateUtilities.ts
@@ -1,3 +1,8 @@
+import dayjs from "dayjs"
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+
 // Converts backend Java timestamps to frontend timestamps
 const javaTimestampToJS = (timestamp: string) => {
     return timestamp?.slice(0, -8)
@@ -9,4 +14,8 @@ const getUSDateStringFromTimestamp = (timestamp: string) => {
     return `${priceStartedDate.getMonth() + 1}/${priceStartedDate.getDate()}/${priceStartedDate.getFullYear()}`
 }
 
-export { javaTimestampToJS, getUSDateStringFromTimestamp }
+const getLocalDateFromUTC = (utcDate: Date) => {
+    return dayjs.utc(utcDate).local()
+}
+
+export { javaTimestampToJS, getUSDateStringFromTimestamp, getLocalDateFromUTC }
