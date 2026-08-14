a4012884a19075144c7825f73e2e8c33e743eb92
christian4747 <christian.andersen4747@gmail.com>
2026-08-14 14:08:58 +0900

    Add return amount to update price

--- price-tracker-backend/src/main/java/com/christian4747/pricetracker/services/PriceService.java
+++ price-tracker-backend/src/main/java/com/christian4747/pricetracker/services/PriceService.java
@@ -109,6 +109,7 @@ public class PriceService {
         priceToUpdate.setPriceStarted(priceDTO.getPriceStarted());
         priceToUpdate.setPriceEnded(priceDTO.getPriceEnded());
         priceToUpdate.setDescription(priceDTO.getDescription());
+        priceToUpdate.setReturnAmount(priceDTO.getReturnAmount());
 
         if (priceToUpdate.getProduct().getProductId() != priceDTO.getProductId()) {
             Optional<Product> existingProduct = productDAO.findById(priceDTO.getProductId());
