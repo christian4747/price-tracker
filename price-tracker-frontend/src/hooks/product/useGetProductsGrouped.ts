ae68b17b3efd3fb565d7d8e49f2c25614b154b93
christian4747 <christian.andersen4747@gmail.com>
2026-08-20 07:47:00 +0900

    Add pagination & count to grouped products hook

--- price-tracker-frontend/src/hooks/product/useGetProductsGrouped.ts
+++ price-tracker-frontend/src/hooks/product/useGetProductsGrouped.ts
@@ -1,16 +1,35 @@
-import { useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import api from '../../services/api'
+import { useState } from 'react'
 
-export function useGetProductsGrouped() {
+export function useGetProductsGrouped(pageNumber: number = 1, pageSize: number = 10) {
+
+    // Current page state
+    const [currentPageNumber, setCurrentPageNumber] = useState(pageNumber)
+    // Current page size state
+    const [currentPageSize, ] = useState(pageSize)
 
     // Query for getting all the products
-    const allProductsQuery = useQuery({
-        queryKey: ['products'],
-        queryFn: api.getProductsGrouped
+    const getProductsGroupedQuery = useQuery({
+        queryKey: ['productsGrouped', currentPageNumber - 1],
+        queryFn: () => {
+            return api.getProductsGrouped(currentPageNumber - 1, currentPageSize)
+        },
+        placeholderData: keepPreviousData
+    })
+
+    const groupedProductCountQuery = useQuery({
+        queryKey: ['productCount'],
+        queryFn: () => {
+            return api.getProductCount()
+        }
     })
 
     const useGetProductsGroupedProps = {
-        query: allProductsQuery
+        query: getProductsGroupedQuery,
+        countQuery: groupedProductCountQuery,
+        currentPageNumber: currentPageNumber,
+        setCurrentPageNumber: setCurrentPageNumber,
     }
 
     return useGetProductsGroupedProps
