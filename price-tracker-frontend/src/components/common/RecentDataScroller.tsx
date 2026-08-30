f19e03e92007b4ed6da96492ddf38523dceafe76
christian4747 <christian.andersen4747@gmail.com>
2026-08-30 11:53:08 +0900

    Add padding to RecentDataScroller

--- price-tracker-frontend/src/components/common/RecentDataScroller.tsx
+++ price-tracker-frontend/src/components/common/RecentDataScroller.tsx
@@ -3,7 +3,7 @@ import { Group, Scroller } from "@mantine/core"
 export const RecentDataScroller = ({ children, ...props }: Scroller.Props) => {
     return (
         <Scroller {...props} >
-            <Group gap="xs" wrap="nowrap">
+            <Group p={1} gap="xs" wrap="nowrap">
                 {children}
             </Group>
         </Scroller>
